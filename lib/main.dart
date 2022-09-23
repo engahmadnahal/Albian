83acd911793c1e86674e8e9a76e03457f8328a08
engahmadnahal <ahmadghssannahal@gmail.com>
2022-09-23 20:29:20 +0300

    Add base file and Packges

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:albian/screens/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Albian',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
+
+
